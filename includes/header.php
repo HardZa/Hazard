50e72d4f9c5d5798b2c4cec126d0f3f2dd6c989b
Nachanon Vetjasit <nachanon@users.noreply.github.com>
2013-12-02 14:28:40 +0700

    changing HTML header to be HTML 4.01 compliant

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title>Hazard and the gang</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+	<title>Hazardous object tracking system</title>
 </head>
 <body>
