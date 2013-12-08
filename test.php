9eaced3883190973a5392415f5d6471ff499de35
ueasaksupa <bossid1999@hotmail.com>
2013-12-08 16:39:12 +0700

    test

--- test.php
+++ test.php
@@ -3,10 +3,7 @@
  require_once('core/init.php');
  include(resolveHeader('includes/header.php'));
 
-echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-// $user = User::get_user();
-// var_dump($user->is_group('client'));
+ include(resolveHeader('includes/forms/vg1.php'));
 
- 
  include(resolveHeader('includes/footer.php'));
  ?>
