1ffab288c813cb0526ae986f4f15dc2c229b8847
serpierio <serpierio@gmail.com>
2013-12-10 12:26:53 +0700

    information in user.php

--- user.php
+++ user.php
@@ -72,13 +72,83 @@ if($user->is_group("registrar"))
 {
     ?>
     
-    <input type="button" value="AddUser" class="btn btn-success" onclick="parent.location='register.php'">
+    <input type="button" value="เพิ่มผู้ใช้ใหม่" class="btn btn-success" onclick="parent.location='register.php'">
+    <br><br>
+    <input type="button" value="จัดการผู้ใช้ในระบบ" class="btn btn-success" onclick="parent.location='register.php'">
 
     <?php
 
 }
+else if($user->is_group("client"))
+{
+    ?>
+    
+    <input type="button" value="ขออนุญาตผลิตวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
+    <br><br>
+    <input type="button" value="ขออนุญาตนำเข้าวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
+    <br><br>
+    <input type="button" value="ขออนุญาตส่งออกวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
+    <br><br>
+    <input type="button" value="ขออนุญาตครอบครองวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
+    <br><br>
+    <input type="button" value="ขอขึ้นทะเบียนวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
+    <br><br>
+    <input type="button" value="ขอต่ออายุทะเบียนวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
+    
+    <?php
 
+}
+else if($user->is_group("hazcontrol"))
+{
+	?>
 
+	<input type="button" value="แสดงคำร้องที่เข้ามาใหม่" class="btn btn-success" onclick="parent.location='register.php'">
+	<br><br>
+	<input type="button" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
+	<br><br>
+	<input type="button" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
+
+	<?php
+
+}
+else if($user->is_group("plantprotection"))
+{
+	?>
+
+	<input type="button" value="แสดงคำร้องที่เข้ามาใหม่" class="btn btn-success" onclick="parent.location='register.php'">
+	<br><br>
+	<input type="button" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">	
+	<br><br>
+	<input type="button" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
+
+	<?php
+
+}
+else if($user->is_group("agriproduction"))
+{
+	?>
+
+	<input type="button" value="แสดงคำร้องที่เข้ามาใหม่" class="btn btn-success" onclick="parent.location='register.php'">
+	<br><br>
+	<input type="button" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">	
+	<br><br>
+	<input type="button" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
+	
+	<?php
+
+}
+else if($user->is_group("cashier"))
+{
+	?>
+
+	<input type="button" value="แสดงคำร้องใหม่ที่รอการชำระเงิน" class="btn btn-success" onclick="parent.location='register.php'">
+	<br><br>
+	<input type="button" value="แสดงคำร้องใหม่ที่รอการตรวจสอบหลักฐานชำระเงิน" class="btn btn-success" onclick="parent.location='register.php'">	
+	<br><br>
+	<input type="button" value="แสดงคำร้องใหม่ที่เกินกำหนดชำระเงิน" class="btn btn-success" onclick="parent.location='register.php'">
+
+	<?php
+}
 
 
 
