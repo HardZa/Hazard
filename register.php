756de22205e0388a7a8e84fd128d84700e623215
HardZa <b5410500164@ku.ac.th>
2013-12-10 18:41:03 +0700

    update register.php , user.php , vg_style.css add cl-logo.256p

--- register.php
+++ register.php
@@ -47,7 +47,8 @@
  
  ?>
  
- <form class="form-horizontal" method="post" action="" role="form" >
+ <form class="form-inline" method="post" action="" role="form" >
+ <!--<form class="form-horizontal" method="post" action="" role="form" >-->
 	 <script type="text/javascript">
  	$(document).ready(function(){
  		$("#regis_type").change(function()
@@ -62,42 +63,67 @@
  		
  	});
  </script>
- 
+
+<div id='content' align="left">
+	<div align="left" class='frame1'>
+	ประเภทของผู้ใช้ :&nbsp;&nbsp;
+	<select name="DateOfBirth_Month" id="regis_type" class='form-control' style="width:250px;height:30px;">
+    	<option value="client">เอกชน</option>
+    	<option value="hazcontrol">เจ้าหน้าที่ควบคุมวัตถุอันตราย</option>
+    	<option value="plantprotection">เจ้าหน้าที่สำนักอารักขาพืช</option>
+    	<option value="agriproduction">เจ้าหน้าที่สำนักปัจจัยการผลิต</option>
+    	<option value="cashier">เจ้าหน้าที่การเงิน</option>
+	</select> 
+	</div>
+
+	<div align="left" class='frame1'>
+
+	<span style="color:#FF0000">*</span>ชื่อล็อกอิน :&nbsp;&nbsp;
+	<input type="text" class="form-control" style="width:100px;height:20px;" placeholder="Username">
+	<span style="color:#848484">(เป็นตัวอักษรภาษาอังกฤษตัวเล็ก, เลข 0-9, หรือสัญลักษณ์ '-','_','.' ยาวไม่เกิน 60 ตัวอักษร)</span><br>
 
 
-<select name="DateOfBirth_Month" id="regis_type">
-    <option value="client">เอกชน</option>
-    <option value="hazcontrol">เจ้าหน้าที่ควบคุมวัตถุอันตราย</option>
-    <option value="plantprotection">เจ้าหน้าที่สำนักอารักขาพืช</option>
-    <option value="agriproduction">เจ้าหน้าที่สำนักปัจจัยการผลิต</option>
-    <option value="cashier">เจ้าหน้าที่การเงิน</option>
-</select> 
-<br><br><br>
-<p align="left">
-ชื่อล็อกอิน:  <input type="text" name="username"> (เป็นตัวอักษรภาษาอังกฤษตัวเล็ก, เลข 0-9, หรือสัญลักษณ์ '-','_','.' ยาวไม่เกิน 60 ตัวอักษร)<br>
-รหัสผ่าน<br>
-ชื่อ-สกุล:   <input type="text" name="name"><br>
-</p>
-<br><br><br><br>
+	<span style="color:#FF0000">*</span>ชื่อ-สกุล :&nbsp;&nbsp;
+	<input type="text" class="form-control" style="width:300px;height:20px;" placeholder="Name"><br>
+
+	</div>
+
+</div>
+
+
+<br><br>
+
+
+
 <!--<button id="but">CLICK</button>-->
 
 
-<div id ="client_form" align="left">
+<div id="client_form" align="left" class='frame1'>
 
-|  วัน-เดือน-ปี เกิด:   <input type="text" name="userbirthdate"> [กรอกเป็น วัน-เลขเดือน-ปีพ.ศ.สี่หลัก] เช่น  31-01-2532 (ไม่ต้องระบุ กรณีที่เป็นนิติบุคคล)<br>
-|  สัญชาติ: <select name="usernationality"><?php include(resolveHeader('includes/forms/element/nationality.php')); ?></select>  (ไม่ต้องระบุ กรณีที่เป็นนิติบุคคล)<br>
-| !เลขประจำตัวผู้เสียภาษี  <input type="text" name="usertaxid">[กรอก: ตัวเลขล้วน]<br>
-| <br>
-| ที่อยู่:<br>
-|&nbsp;&nbsp;&nbsp;&nbsp;!บ้านเลขที่  <input type="text" name="useraddrhouse"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;หมู่ที่      <input type="text" name="useraddrvillage"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;ตรอก/ซอย <input type="text" name="userdrive"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;!ถนน <input type="text" name="useraddrroad"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;!ตำบล/แขวง <input type="text" name="usersubdistrict"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;!จังหวัด <input type="text" name="userprovince"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;!รหัสไปรษณีย์ <input type="text" name="userpostalcode"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;!หมายเลขโทรศัพท์ <input type="text" name="userphone"><br>
-|&nbsp;&nbsp;&nbsp;&nbsp;หมายเลขโทรสาร <input type="text" name="userfax"><br>
+  	วัน-เดือน-ปี เกิด :&nbsp;&nbsp;
+  	<input type="text" name="userbirthdate" class="form-control" style="width:300px;height:20px;"> 
+  	<span style="color:#848484">[กรอกเป็น วัน-เลขเดือน-ปีพ.ศ.สี่หลัก] เช่น  31-01-2532 (ไม่ต้องระบุ กรณีที่เป็นนิติบุคคล)</span><br>
+
+  	สัญชาติ :&nbsp;&nbsp;
+  	<select name="usernationality"><?php include(resolveHeader('includes/forms/element/nationality.php')); ?></select>  
+  	<span style="color:#848484">(ไม่ต้องระบุ กรณีที่เป็นนิติบุคคล)</span	><br>
+	
+	<span style="color:#FF0000">*</span>เลขประจำตัวผู้เสียภาษี :&nbsp;&nbsp; 
+	<input type="text" name="usertaxid" class="form-control" style="width:300px;height:20px;">
+	<span style="color:#848484">[กรอก: ตัวเลขล้วน]</span><br>
+
+ 	<br>
+
+ 	ที่อยู่:<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#FF0000">*</span>บ้านเลขที่ :&nbsp;&nbsp;<input type="text" name="useraddrhouse" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;หมู่ที่ :&nbsp;&nbsp;<input type="text" name="useraddrvillage" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;ตรอก/ซอย :&nbsp;&nbsp;<input type="text" name="userdrive" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#FF0000">*</span>ถนน :&nbsp;&nbsp;<input type="text" name="useraddrroad" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#FF0000">*</span>ตำบล/แขวง :&nbsp;&nbsp;<input type="text" name="usersubdistrict" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#FF0000">*</span>จังหวัด :&nbsp;&nbsp;<input type="text" name="userprovince" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#FF0000">*</span>รหัสไปรษณีย์ :&nbsp;&nbsp;<input type="text" name="userpostalcode" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#FF0000">*</span>หมายเลขโทรศัพท์ :&nbsp;&nbsp;<input type="text" name="userphone" class="form-control" style="width:300px;height:20px;"><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;หมายเลขโทรสาร :&nbsp;&nbsp;<input type="text" name="userfax" class="form-control" style="width:300px;height:20px;"><br>
 
 </div>
 </form>
--- user.php
+++ user.php
@@ -71,10 +71,28 @@ $user = User::get_user();
 if($user->is_group("registrar"))
 {
     ?>
-    
+    <table border = 0 align="center">
+    	<tr>
+   		<td>
+    		<a href="register.php" style="width:256px">
+        		<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="เพิ่มผู้ใช้ใหม่">
+        		<br>เพิ่มผู้ใช้ใหม่
+    		</a>
+    		
+    	</td>
+		<td>
+    		<a href="register.php" style="width:256px">
+        		<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="จัดการผู้ใช้ในระบบ">
+        		<br>จัดการผู้ใช้ในระบบ
+    		</a>
+    	</td>
+    	</tr>    
+	</table>
+    <!--
     <input type="button" value="เพิ่มผู้ใช้ใหม่" class="btn btn-success" onclick="parent.location='register.php'">
     <br><br>
     <input type="button" value="จัดการผู้ใช้ในระบบ" class="btn btn-success" onclick="parent.location='register.php'">
+	-->
 
     <?php
 
@@ -82,7 +100,56 @@ if($user->is_group("registrar"))
 else if($user->is_group("client"))
 {
     ?>
+    <table border = 0 align="center">
+    <tr>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="ขออนุญาตผลิตวัตถุอันตราย">	
+                	<br>ขออนุญาตผลิตวัตถุอันตราย
+                </a>
+    </td>        	
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="ขออนุญาตนำเข้าวัตถุอันตราย">
+                	<br>ขออนุญาตนำเข้าวัตถุอันตราย
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="ขออนุญาตส่งออกวัตถุอันตราย">
+                	<br>ขออนุญาตส่งออกวัตถุอันตราย
+                </a>            
+    </td>
+	</tr>
+	<tr>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="ขออนุญาตครอบครองวัตถุอันตราย">	
+                	<br>ขออนุญาตครอบครองวัตถุอันตราย
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="ขอขึ้นทะเบียนวัตถุอันตราย">
+                	<br>ขอขึ้นทะเบียนวัตถุอันตราย
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="ขอต่ออายุทะเบียนวัตถุอันตราย">
+                	<br>ขอต่ออายุทะเบียนวัตถุอันตราย
+                </a>            
+    </td>
+            <!--
+            <div style="display: inline">
+                <a href="#" class="thumbnail" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.png");  ?>">
+                </a>
+            </div>
+            -->
+        
     
+    <!--
     <input type="button" value="ขออนุญาตผลิตวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
     <br><br>
     <input type="button" value="ขออนุญาตนำเข้าวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
@@ -94,79 +161,146 @@ else if($user->is_group("client"))
     <input type="button" value="ขอขึ้นทะเบียนวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
     <br><br>
     <input type="button" value="ขอต่ออายุทะเบียนวัตถุอันตราย" class="btn btn-success" onclick="parent.location='register.php'">
-    
+    -->
     <?php
 
 }
 else if($user->is_group("hazcontrol"))
 {
 	?>
+    <table border = 0 align="center">
+    <tr>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่เข้ามาใหม่">	
+                	<br>แสดงคำร้องที่เข้ามาใหม่
+                </a>
+    </td>        	
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่">
+                	<br>แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่">
+                	<br>แสดงคำร้องที่ฉันรับดำเนินการอยู่
+                </a>            
+    </td>
+	</tr>
 
+<!--
 	<input type="button" value="แสดงคำร้องที่เข้ามาใหม่" class="btn btn-success" onclick="parent.location='register.php'">
 	<br><br>
 	<input type="button" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
 	<br><br>
 	<input type="button" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
-
+-->
 	<?php
 
 }
 else if($user->is_group("plantprotection"))
 {
 	?>
-
+    <table border = 0 align="center">
+    <tr>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่เข้ามาใหม่">	
+                	<br>แสดงคำร้องที่เข้ามาใหม่
+                </a>
+    </td>        	
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่">
+                	<br>แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่">
+                	<br>แสดงคำร้องที่ฉันรับดำเนินการอยู่
+                </a>            
+    </td>
+	</tr>
+<!--
 	<input type="button" value="แสดงคำร้องที่เข้ามาใหม่" class="btn btn-success" onclick="parent.location='register.php'">
 	<br><br>
 	<input type="button" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">	
 	<br><br>
 	<input type="button" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
-
+-->
 	<?php
 
 }
 else if($user->is_group("agriproduction"))
 {
 	?>
-
+    <table border = 0 align="center">
+    <tr>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่เข้ามาใหม่">	
+                	<br>แสดงคำร้องที่เข้ามาใหม่
+                </a>
+    </td>        	
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่">
+                	<br>แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่">
+                	<br>แสดงคำร้องที่ฉันรับดำเนินการอยู่
+                </a>            
+    </td>
+	</tr>
+<!--
 	<input type="button" value="แสดงคำร้องที่เข้ามาใหม่" class="btn btn-success" onclick="parent.location='register.php'">
 	<br><br>
 	<input type="button" value="แสดงคำร้องที่ฝ่ายรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">	
 	<br><br>
 	<input type="button" value="แสดงคำร้องที่ฉันรับดำเนินการอยู่" class="btn btn-success" onclick="parent.location='register.php'">
-	
+-->
 	<?php
 
 }
 else if($user->is_group("cashier"))
 {
 	?>
-
+    <table border = 0 align="center">
+    <tr>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องใหม่ที่รอการชำระเงิน">	
+                	<br>แสดงคำร้องใหม่ที่รอการชำระเงิน
+                </a>
+    </td>        	
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องใหม่ที่รอการตรวจสอบหลักฐานชำระเงิน">
+                	<br>แสดงคำร้องใหม่ที่รอการตรวจสอบหลักฐานชำระเงิน
+                </a>
+    </td>
+    <td>
+                <a href="register.php" style="width:256px">
+                	<img src="<?php echo resolveURIHeader("image/cl-logo.256p.png");  ?>" value="แสดงคำร้องใหม่ที่เกินกำหนดชำระเงิน">
+                	<br>แสดงคำร้องใหม่ที่เกินกำหนดชำระเงิน
+                </a>            
+    </td>
+	</tr>
+<!--
 	<input type="button" value="แสดงคำร้องใหม่ที่รอการชำระเงิน" class="btn btn-success" onclick="parent.location='register.php'">
 	<br><br>
 	<input type="button" value="แสดงคำร้องใหม่ที่รอการตรวจสอบหลักฐานชำระเงิน" class="btn btn-success" onclick="parent.location='register.php'">	
 	<br><br>
 	<input type="button" value="แสดงคำร้องใหม่ที่เกินกำหนดชำระเงิน" class="btn btn-success" onclick="parent.location='register.php'">
-
+-->
 	<?php
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
