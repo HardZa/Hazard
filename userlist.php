ef27631a6676d59919a27cc8887f583ba0ca8c06
Pea Pakawat <priest_smilly5@hotmail.com>
2013-12-13 02:17:14 +0700

    userlist.php, edituser.php

---
+++ userlist.php
@@ -0,0 +1,46 @@
+<?php
+require_once('core/init.php');
+include(resolveHeader('includes/header.php'));
+
+?>
+
+<div class="page-header">
+    <h1>รายการผู้ใช้</h1>
+</div>
+
+<table class="table">
+<tr>
+<th>ชื่อ-สกุล</th>
+<th>ประเภทผู้ใช้</th>
+<th>แก้ไข</th>
+</tr>
+
+
+
+<?php
+for($i=1;(DB::get_db()->select('users',null,'userid='.$i,1))!=null;$i++)
+{
+
+	$row = DB::get_db()->select('users',null,'userid='.$i,1);
+	$user = $row[0];
+	$name = $user['username'];
+	$type = User::get_group_by_id($user['userid']);
+	$url = "parent.location='edituser.php?user=";
+
+
+	echo "<tr>
+<td>$name</td>
+<td>$type</td>
+<td><button type='button' onclick=$url".$i."' >แก้ไข</button></td>
+</tr>";
+}
+
+?>
+
+</table>
+
+<?php
+
+
+include(resolveHeader('includes/footer.php'));
+?>
