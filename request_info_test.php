b5962b3310af43510514eaa07191ab4ed80d2ed1
ueasaksupa <bossid1999@hotmail.com>
2013-12-13 02:36:32 +0700

    add test request flow

---
+++ request_info_test.php
@@ -0,0 +1,50 @@
+<?php
+ 	require_once('core/init.php');
+	include(resolveHeader('includes/header.php'));
+?>
+<section class="top-sec">
+	<div class="container">
+		<div class="request-flow">
+			<div class="dot1">
+				<img src="<?php echo resolveURIHeader("image/dot1.png");?>"></img>
+			</div>
+			<div class="dot2">
+				<img src="<?php echo resolveURIHeader("image/dot1.png");?>"></img>
+			</div>
+			<div class="dot3">
+				<img src="<?php echo resolveURIHeader("image/dot1.png");?>"></img>
+			</div>
+			<div class="dot4">
+				<img src="<?php echo resolveURIHeader("image/dot1.png");?>"></img>
+			</div>
+			<div class="dot5">
+				<img src="<?php echo resolveURIHeader("image/dot1.png");?>"></img>
+			</div>
+			<div class="dot6">
+				<img src="<?php echo resolveURIHeader("image/dot1.png");?>"></img>
+			</div>
+
+			<div class="flow-msg1">
+				Test
+			</div>
+			<div class="flow-msg2">
+				Test1
+			</div>
+			<div class="flow-msg3">
+				Test2
+			</div>
+			<div class="flow-msg4">
+				Test3
+			</div>
+			<div class="flow-msg5">
+				Test4
+			</div>
+			<div class="flow-msg6">
+				Test5
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+ 	include(resolveHeader('includes/footer.php'));
+?>
