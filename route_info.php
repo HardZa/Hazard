13c73a19784663d3be864008b7830c03133d4ca4
Art <nattapat.art@gmail.com>
2013-12-14 00:36:36 +0700

    ++ file

--- route_info.php
+++ route_info.php
@@ -7,6 +7,7 @@ if($_chktype == VO_GS_GVG_1)
 	$route_file = 'vo_gs_gvg_1.php';
 }
 
+include(resolveHeader(Config::get('template/bind_info').$route_file));
 include(resolveHeader(Config::get('template/request_info').$route_file));
 
 ?>
