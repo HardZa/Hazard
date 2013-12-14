81c81d7200acf8fe9f322e8e3bdc4d73a807cafd
ueasaksupa <bossid1999@hotmail.com>
2013-12-14 16:11:08 +0700

    Edit request info page

--- request_info.php
+++ request_info.php
@@ -45,49 +45,56 @@
  $cexp = get_exp_cert();
 
  ?>
-<div class="container">
-<div class="page-header">
-    <h1>
-    		<?php echo $request->get_type(); ?>
-    		(<?php echo $request->get_status(); ?>)
-    		<?php echo $cexp; ?>
-    </h1>
-</div>
- <div class="row">
-    	<div class="col-sm-6">
-    		<img src="<?php echo resolveURIHeader( $request->get_img_uri() ); ?>"></img>
-	    </div>
- </div>
+<section class="top-sec">
+	<div class="container" style="width:1024px;">
+		<div class="request-flow">
+			<div>
+				<div class="arrow_box">
+					<h4>
+						<span class="glyphicon glyphicon-search"></span>
+						(<?php echo $request->get_status();?>)
+						<?php echo $cexp;?>
+					</h4>
+				</div>
+			</div>
+			<div class="flow-msg-head">
+				<h4>สถานะคำร้อง <?php echo $request->get_type(); ?> </h4>
+			</div>
+			<img src="<?php echo resolveURIHeader( $request->get_img_uri() ); ?>"></img>
+		</div>
+	</div>	
+</section>
+<section>
+	<div class="container">
+		<div class="row">
+		<?php
+		$docData = array('progress' => $request->get('progress'),
+						  'main_file' => 'request_info.php',
+						  'request' => $request ,
+						  'type' => $request->get('requesttype') ,
+						  'user' => $user);
+		include( resolveHeader('route_info.php') );
+		?>
+		</div>
 
- <div class="row">
- <?php
- $docData = array('progress' => $request->get('progress'),
- 				  'main_file' => 'request_info.php',
- 				  'request' => $request ,
- 				  'type' => $request->get('requesttype') ,
- 				  'user' => $user);
- include( resolveHeader('route_info.php') );
- ?>
- </div>
+		<?php
+			printPic(PIC_DOC_ATTACH);
+			printPic(PIC_TEST_ATTACH);
+			printPic(PIC_BILL);
+		?>
 
- <?php
- 	printPic(PIC_DOC_ATTACH);
- 	printPic(PIC_TEST_ATTACH);
- 	printPic(PIC_BILL);
- ?>
-
- <div class="row">
- <?php
- $docData = array('progress' => $request->get('progress'),
- 				  'main_file' => 'request_info.php',
- 				  'request' => $request ,
- 				  'type' => $request->get('requesttype') ,
- 				  'user' => $user);
- include( resolveHeader('route_show_request.php') );
- ?>
- </div>
-</div>
-
+		<div class="row">
+		<?php
+		$docData = array('progress' => $request->get('progress'),
+						  'main_file' => 'request_info.php',
+						  'request' => $request ,
+						  'type' => $request->get('requesttype') ,
+						  'user' => $user);
+		include( resolveHeader('route_show_request.php') );
+		?>
+		</div>
+	</div>
+</section>	
 
  <?php
  include(resolveHeader('includes/footer.php'));
