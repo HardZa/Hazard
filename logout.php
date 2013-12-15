8df0a4be9cb04f8cf928e85ce07451011a99db41
Nachanon Vetjasit <nachanon@users.noreply.github.com>
2013-12-15 17:45:15 +0700

    simplifying logout script, correcting logout redirect

--- logout.php
+++ logout.php
@@ -1,15 +1,10 @@
- <?php
- 
- require_once('core/init.php');
- User::clear_user();
- include(resolveHeader('includes/header.php'));
- 
- ?>
- 
- <?php
- 	Redirect::to('/');
- ?>
- 
-  <?php
- include(resolveHeader('includes/footer.php'));
- ?>
+<?php
+
+require_once('core/init.php');
+User::clear_user();
+include(resolveHeader('includes/header.php'));
+
+Redirect::to('');
+
+include(resolveHeader('includes/footer.php'));
+?>
